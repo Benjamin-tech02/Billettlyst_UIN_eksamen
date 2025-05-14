06ae49437cce82bdf43936b6923b72f0f3edb637
Benjamin-tech02 <yamena@hiof.no>
2025-05-14 22:48:48 +0200

    fullført home komponent (E)
    Vi har importert useEffect og useState som bruker fetch funksjonen til å legge eventsa i en liste, pluss fetchAllFestivals som henter festivalene fra ticketmastter ved bruk av ID og importerte Link fra react router dom, for å bruker Link til "les mer om ......."

---
+++ FrontendReact/src/components/Home.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+import { fetchAllFestivals } from '../api/ticketmaster';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    fetchAllFestivals().then(setEvents);
+    }, []);
+
+  return (
+    <main>
+      <h1>Sommerens festivaler!</h1>
+      <section>
+        {events.map(event => (
+          <article key={event.id}>
+            {event.images?.[0]?.url && (
+              <img src={event.images[0].url} alt={event.name} />
+            )}
+            <h3>{event.displayName}</h3>
+            <Link to={/event/${event.id}}>
+              <button>Les mer om {event.displayName}</button>
+            </Link>
+          </article>
+        ))}
+      </section>
+    </main>
+  );
+};
+
+export default Home;
