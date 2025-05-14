d170d2b214c938e59cf5fc03c5e858346be28fdd
Benjamin-tech02 <yamena@hiof.no>
2025-05-14 23:26:04 +0200

    fullført CategoryPage (E)
    vi har lagd en side som sender brukeren til kategorien som brukeren trykker på, om det f. eks sport, musikk eller teater ved bruk av slug. vi bruker fetchEventsByKeyword for å hente alle eventsa som har samme navn som slug-en

---
+++ FrontendReact/src/components/CategoryPage.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchEventsByKeyword } from '../api/ticketmaster';
+
+const CategoryPage = () => {
+    const { slug } = useParams();
+    const [events, setEvents] = useState([]);
+    const [setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const eventsData = await fetchEventsByKeyword(slug);
+                setEvents(eventsData);
+            } catch (err) {
+                setError('Feil under henting av arrangementer.');
+                console.error(err);
+            }
+        };
+
+        fetchData();
+    }, [slug]);
+
+    return (
+        <section>
+            <h1>{slug}</h1>
+            <article>
+                {events.map((event) => (
+                    <div key={event.id}>
+                        {event.images?.[0]?.url && (
+                            <img src={event.images[0].url} alt={event.name} />
+                            )
+                        }
+                        <h2>{event.name}</h2>
+                        <time>{event.dates?.start?.localDate}</time>
+                    </div>
+                    ))
+                }
+            </article>
+        </section>
+    );
+};
+
+export default CategoryPage;
