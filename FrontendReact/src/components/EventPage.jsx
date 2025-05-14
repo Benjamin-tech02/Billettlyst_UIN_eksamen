24026ae709e3200255362fe9141bd910692ef837
Benjamin-tech02 <yamena@hiof.no>
2025-05-15 00:01:03 +0200

    fullført EventPage (E)
    vi har lagd en komponent som sender brukeren til event siden når "les mer om" knappen trykkes. på siden til eventen vises det bilde av festivalen, navnet og annen info ved å hente dataene til eventen som ble trukket på ved bruk av id-en.

---
+++ FrontendReact/src/components/EventPage.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchAllFestivals } from '../api/ticketmaster';
+
+const EventPage = () => {
+    const { id } = useParams(); 
+    const [event, setEvent] = useState(null);
+    const [setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const events = await fetchAllFestivals();
+                const eventData = events.find(event => event.id === id);
+
+                if (eventData) {
+                setEvent(eventData);
+                } else {
+                setError('Kunne ikke finne festival med denne ID-en.');
+                }
+            } catch (err) {
+                setError('Feil under henting av festivaldata');
+                console.error(err);
+            } 
+        };
+
+        fetchData();
+    }, [id]);
+
+    return (
+        <article>
+            <h1>{event.displayName}</h1>
+            {event.images?.[0]?.url && (
+                <img src={event.images[0].url} alt={event.name} />
+            )}
+            <p>{event.dates?.start?.localDate}</p>
+            <p>{event.dates?.start?.localTime}</p>
+            <p>Sted: {event._embedded?.venues?.[0]?.name}</p>
+        </article>
+    );
+};
+
+export default EventPage;
