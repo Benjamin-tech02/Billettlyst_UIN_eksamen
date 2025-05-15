7b7e5a90a2d61e426942fcc373ab6fcd220a94d3
Benjamin-tech02 <yamena@hiof.no>
2025-05-15 16:09:45 +0200

    Update EventCard.jsx

--- FrontendReact/src/components/EventCard.jsx
+++ FrontendReact/src/components/EventCard.jsx
@@ -3,21 +3,21 @@ import { Link } from 'react-router-dom';
 const EventCard = ({ event, clickable = true }) => {
     if (!event) return null;
 
-  return (
-    <article>
-        {event.images?.[0]?.url && (
-            <img src={event.images[0].url} alt={event.name} />
-        )}
-        <h3>{event.displayName || event.name}</h3>
-        <p>{event.dates?.start?.localDate}</p>
+    return (
+        <article>
+            {event.images?.[0]?.url && (
+                <img src={event.images[0].url} alt={event.name} />
+            )}
+            <h3>{event.displayName || event.name}</h3>
+            <p>{event.dates?.start?.localDate}</p>
 
-        {clickable && (
-            <Link to={`/event/${event.id}`}>
-                <button>Les mer om {event.displayName || event.name}</button>
-            </Link>
-        )}
-    </article>
-  );
+            {clickable && (
+                <Link to={`/event/${event.id}`}>
+                    <button>Les mer om {event.displayName || event.name}</button>
+                </Link>
+            )}
+        </article>
+    );
 };
 
 export default EventCard;
