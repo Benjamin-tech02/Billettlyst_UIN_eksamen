b910fb57df6aa7806f2e7fe2870231dddfb88dee
Benjamin-tech02 <yamena@hiof.no>
2025-05-15 15:43:37 +0200

    fullført EventCard (D)
    Vi har opprettet EventCard komponent for å kunne bruke den i Home, CategoryPage og Dashboard komponent (senere).

---
+++ FrontendReact/src/components/EventCard.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+
+const EventCard = ({ event, clickable = true }) => {
+    if (!event) return null;
+
+  return (
+    <article>
+        {event.images?.[0]?.url && (
+            <img src={event.images[0].url} alt={event.name} />
+        )}
+        <h3>{event.displayName || event.name}</h3>
+        <p>{event.dates?.start?.localDate}</p>
+
+        {clickable && (
+            <Link to={`/event/${event.id}`}>
+                <button>Les mer om {event.displayName || event.name}</button>
+            </Link>
+        )}
+    </article>
+  );
+};
+
+export default EventCard;
