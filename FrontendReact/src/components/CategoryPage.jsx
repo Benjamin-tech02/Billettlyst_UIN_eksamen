54b9895340cb32d2cb58a477ad81d38b583f3d31
Benjamin-tech02 <yamena@hiof.no>
2025-05-15 16:13:08 +0200

    Update CategoryPage.jsx

--- FrontendReact/src/components/CategoryPage.jsx
+++ FrontendReact/src/components/CategoryPage.jsx
@@ -4,30 +4,30 @@ import { fetchEventsByKeyword } from '../api/Ticketmaster';
 import EventCard from './EventCard';
 
 const CategoryPage = () => {
-  const { slug } = useParams();
-  const [events, setEvents] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-        try {
-            const eventsData = await fetchEventsByKeyword(slug);
-            setEvents(eventsData);
-        } catch (err) {
-            console.error('Feil under henting av arrangementer:', err);
-        }
+    const { slug } = useParams();
+    const [events, setEvents] = useState([]);
+    
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const eventsData = await fetchEventsByKeyword(slug);
+                setEvents(eventsData);
+            } catch (err) {
+                console.error('Feil under henting av arrangementer:', err);
+            }
     };
 
     fetchData();
-  }, [slug]);
+}, [slug]);
 
-  return (
-    <section>
-        <h1>{slug}</h1>
-        {events.map((event) => (
-            <EventCard key={event.id} event={event} clickable={false} />
-        ))}
-    </section>
-  );
+    return (
+        <section>
+            <h1>{slug}</h1>
+            {events.map((event) => (
+                <EventCard key={event.id} event={event} clickable={false} />
+            ))}
+        </section>
+    );
 };
 
 export default CategoryPage;
