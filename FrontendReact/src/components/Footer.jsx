9d524de8f244f6a2b1ae20da1329592b3382e713
Benjamin-tech02 <yamena@hiof.no>
2025-05-16 15:57:26 +0200

    lage Footer komponent
    lage footer og legge til attributions link til api-et i footeren

---
+++ FrontendReact/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+      <footer>
+        <p>
+          Attribution link: <a href="https://developer.ticketmaster.com/" target="_blank" rel="noopener noreferrer">Ticketmaster API</a>
+        </p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
