2271ba273219dc6b4be9739fab66b205c6c24cf7
Benjamin-tech02 <yamena@hiof.no>
2025-05-16 17:14:36 +0200

    kommentar

--- FrontendReact/src/api/Ticketmaster.js
+++ FrontendReact/src/api/Ticketmaster.js
@@ -20,6 +20,27 @@ export const fetchEventById = async (id) => {
     }
 };
 
+// kilde 1: brukte ChatGPT, beskrevet i eksamensdokumentet.
+/*
+    Vi lagde funkskonen fetchallfestivls slik vi så for oss at den skulle være. 
+    Vi fikk CORS-feil og forhørte oss med en bekjent som har hatt faget før. 
+    Denne feilen var visst en gjenganger med henting av API
+    Deretter sendte vi funksjonen med erroren vi fikk i konsollen til ChatGPT.
+    Først fikk vi en løsning med å bruke timer på kall av hvert api eventid som forsinker prosessen.
+    Vi fikk fremdeles error, og etterspurte ny løsning, hvor vi fikk løsningen som lagret informasjonen 
+    direkte i nettleserens "storage".
+
+    1-Funksjonen lar oss sjekke om dataen allerede finnes lokalt i nettleseren, for å unngå 
+    å kalle api-et hvergang vi laster inn siden. Hvis cache finnes, bruker vi den og avslutter funksjonen tidlig.
+    2-Funksjonen går gjennom alle festivalene vi har satt opp manuelt med id og name.
+    3-Bruke fetchEventById() til å hente festivalene fra Ticketmaster api-et basert på id.
+    4-Vi sjekker at dataen ser riktig ut (at events finnes, har bilder osv). hvis alt 
+    ser riktig ut, legger vi det results arrayen med riktig navn (displayName). hvis det mangler
+    data, så hopper vi over det og returnerer error.
+    5-Vi pauser 100ms for å unngå å spamme api-et og dermed få CORS error.
+    6-Vi lagrer dataen i localstorage.
+*/
+
 export const fetchAllFestivals = async () => {
     const cached = localStorage.getItem("festivalCache");
     if (cached) {
