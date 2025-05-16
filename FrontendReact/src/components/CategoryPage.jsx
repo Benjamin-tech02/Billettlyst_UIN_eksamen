90f8e3ebecefe860538cde01e8fedb4fdaea74ee
Benjamin-tech02 <yamena@hiof.no>
2025-05-16 14:36:33 +0200

    liten endring
    Vi har lagt til en div for å kunne endre på stilen til artikkelen uten å påvirke h2 taggen

--- FrontendReact/src/components/CategoryPage.jsx
+++ FrontendReact/src/components/CategoryPage.jsx
@@ -4,30 +4,32 @@ import { fetchEventsByKeyword } from '../api/Ticketmaster';
 import EventCard from './EventCard';
 
 const CategoryPage = () => {
-  const { slug } = useParams();
-  const [events, setEvents] = useState([]);
+    const { slug } = useParams();
+    const [events, setEvents] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
+    useEffect(() => {
+        const fetchData = async () => {
         try {
             const eventsData = await fetchEventsByKeyword(slug);
             setEvents(eventsData);
         } catch (err) {
             console.error('Feil under henting av arrangementer:', err);
         }
-    };
+        };
 
-    fetchData();
-  }, [slug]);
+        fetchData();
+    }, [slug]);
 
-  return (
-    <section>
-        <h1>{slug}</h1>
-        {events.map((event) => (
-            <EventCard key={event.id} event={event} clickable={false} />
-        ))}
-    </section>
-  );
+    return (
+        <section className="category-seksjon">
+            <h2>{slug}</h2>
+            <div>
+                {events.map((event) => (
+                    <EventCard key={event.id} event={event} clickable={false} />
+                ))}
+            </div>
+        </section>
+    );
 };
 
 export default CategoryPage;
