167e48a37e4069fe45dc37f27fe2b8cf114987f3
Benjamin-tech02 <yamena@hiof.no>
2025-05-16 16:01:03 +0200

    legge til footer

--- FrontendReact/src/App.jsx
+++ FrontendReact/src/App.jsx
@@ -4,20 +4,22 @@ import EventPage from './components/EventPage';
 import CategoryPage from './components/CategoryPage';
 import Dashboard from './components/Dashboard';
 import Header from './components/Header';
-import './styles/style.css';
+import Footer from './components/Footer';
+import './styles/style.css'
 
 function App() {
-    return (
-      <Router>
-        <Header/>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/event/:id" element={<EventPage />} />
-          <Route path="/category/:slug" element={<CategoryPage />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </Router>
-    );
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/event/:id" element={<EventPage />} />
+        <Route path="/category/:slug" element={<CategoryPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
