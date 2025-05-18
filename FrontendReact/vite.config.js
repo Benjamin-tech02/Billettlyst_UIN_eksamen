7d96443f75bee1a8da85a8f1da6fd9f68994cbf9
Benjamin-tech02 <yamena@hiof.no>
2025-05-18 23:46:43 +0200

    kommentar kilde 1

--- FrontendReact/vite.config.js
+++ FrontendReact/vite.config.js
@@ -1,3 +1,11 @@
+// Kilde 1
+// lage en lokal proxy for utviklingsserveren
+// Koden endrer API-adressen https://app.ticketmaster.com/discovery/v2/events 
+// til /api/discovery/v2/events. på den måten kan vi bruke /api/discovery/v2/eventsa i 
+// fetch funksjonen for å hente data, isteden for å bruke selve API-adressen
+// på denne måten unngår CORS-problemer når vi prøver å gjøre API-kall til 
+// ticketmaster.com fra vår lokale frontend og Skjuler API-nøkkelen i 
+// frontendkoden, ved å sende kall via /api.
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
